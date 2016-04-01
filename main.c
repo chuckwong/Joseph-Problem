bc74b569587af51362ed18bbe46120d05aa126ba
chuck <14jhwang@stu.edu.cn>
2016-04-01 08:51:52 +0800

    Add Circular Link

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 
 #define N 10
-#define M 9
+#define M 3
 #define K 0
 
 struct node {
@@ -14,48 +14,63 @@ struct node {
     struct node *next;
 };
 
-void InitNodes(struct node **first);
-void DisplayNodes(struct node *first);
+void InitNodes(struct node **head);
+void DisplayNodes(struct node *head);
+void DeleteNode(struct node **head, struct node *d);
+void SolveByRecursion(struct node **head, int m);
 
 // main
 int main(int argc, char **argv)
 {
-    struct node *first;
+    struct node *head;
 
-    InitNodes(&first);
-    // if (first == NULL)
+    InitNodes(&head);
+    // if (head == NULL)
     //     printf("NULL\n")
-    DisplayNodes(first);
+    DisplayNodes(head);
+    SolveByRecursion(&head, M);
 
     return 0;
 }
 
 // InitNodes
-void InitNodes(struct node **first)
+void InitNodes(struct node **head)
 {
-    *first = (struct node *)malloc(sizeof(struct node));
-    struct node *p = *first;
+    *head = (struct node *)malloc(sizeof(struct node));
+    struct node *p = *head;
     p->number = 0;
     int i = 1;
     while (i < N) {
         p->next = (struct node *)malloc(sizeof(struct node));
         p->next->number = i;
-        if (i == N - 1) // must assign the next ptr to NULL for the last node
-            p->next->next = NULL;
+        if (i == N - 1) // must assign the next ptr to HEAD for the last node
+            p->next->next = *head;
         p = p->next;
         i++;
     }
 }
 
 // DisplayNodes
-void DisplayNodes(struct node *first)
+void DisplayNodes(struct node *head)
 {
-    struct node *p = first;
-    while (p) {
+    struct node *p = head;
+    int i = 0;
+    while (p && i < N) {
         printf("%d   ", p->number);
         p = p->next;
+        i++;
     }
     printf("\n");
 }
 
+// DeleteNode
+void DeleteNode(struct node **head, struct node *d)
+{
 
+}
+
+// SolveByRecursion
+void SolveByRecursion(struct node **head, int m)
+{
+    struct node *p = *head;
+}
