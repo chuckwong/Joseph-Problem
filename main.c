4c0cf263575aa0d6d1939335c493377ad8404f00
chuck <14jhwang@stu.edu.cn>
2016-04-01 08:43:13 +0800

    Restore and Finish Display and Initialization

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define N 30
+#define N 10
 #define M 9
 #define K 0
 
@@ -14,7 +14,7 @@ struct node {
     struct node *next;
 };
 
-void InitNodes(struct node *first);
+void InitNodes(struct node **first);
 void DisplayNodes(struct node *first);
 
 // main
@@ -22,22 +22,26 @@ int main(int argc, char **argv)
 {
     struct node *first;
 
-    InitNodes(first);
+    InitNodes(&first);
+    // if (first == NULL)
+    //     printf("NULL\n")
     DisplayNodes(first);
 
     return 0;
 }
 
 // InitNodes
-void InitNodes(struct node *first)
+void InitNodes(struct node **first)
 {
-    // first = (struct node *)malloc(sizeof(struct node));
-    struct node *p;
-    p = first;
-    int i = 0;
+    *first = (struct node *)malloc(sizeof(struct node));
+    struct node *p = *first;
+    p->number = 0;
+    int i = 1;
     while (i < N) {
-        p = (struct node *)malloc(sizeof(struct node));
-        p->number = i;
+        p->next = (struct node *)malloc(sizeof(struct node));
+        p->next->number = i;
+        if (i == N - 1) // must assign the next ptr to NULL for the last node
+            p->next->next = NULL;
         p = p->next;
         i++;
     }
@@ -46,10 +50,9 @@ void InitNodes(struct node *first)
 // DisplayNodes
 void DisplayNodes(struct node *first)
 {
-    struct node *p;
-    p = first;
+    struct node *p = first;
     while (p) {
-        printf("%d  ", p->number);
+        printf("%d   ", p->number);
         p = p->next;
     }
     printf("\n");
